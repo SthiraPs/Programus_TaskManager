6d5635c97e4d82e98cc35780c1792aaf6c7458ab
Sithira Senanayake <sithirasenanayake@gmail.com>
2024-02-01 13:02:20 +0530

    Initial backend project added
    Programus SSE interview

---
+++ Backend/Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
---
+++ Backend/Backend/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        [BindNever]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+    }
+}
